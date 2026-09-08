8aded7bea85139df46b863db919e6ed2744d4737
Constantine Y <kostiklinda7@gmail.com>
2026-09-08 11:23:15 +0300

    feat: mount global language and theme preferences

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import {GlobalPreferenceControls, UiPreferencesProvider} from './uiPreferences';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <UiPreferencesProvider>
+      <App />
+      <GlobalPreferenceControls />
+    </UiPreferencesProvider>
   </StrictMode>,
 );
