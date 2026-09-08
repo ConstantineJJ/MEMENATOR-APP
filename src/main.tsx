554ab07274c29a43d23a9f97ec0604488cc299ae
Constantine Y <kostiklinda7@gmail.com>
2026-09-08 18:41:00 +0300

    mobile: sync responsive profile at app startup

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,10 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import {GlobalPreferenceControls, UiPreferencesProvider} from './uiPreferences';
 import {PartisanLocalizationCleanup} from './partisanLocalization';
+import {startResponsiveProfileSync} from './responsiveProfiles';
 import './index.css';
+
+startResponsiveProfileSync();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
